e3a599009d14bea14b27a0daeb083fdd0d420b1b
BigD <dlau373@gmail.com>
2018-12-07 17:51:54 -0800

    Working on new_year_chaos function

--- hacker_rank/new_year_chaos.js
+++ hacker_rank/new_year_chaos.js
@@ -1,12 +1,20 @@
-function minimum_bribes(q){
+function minimumBribes(q){
+  let minBribes=0;
   let initialQueue=[...q].sort((a,b)=>a-b);
   for (let i=0; i<initialQueue.length;i++){
     let value=q[i]
-    if (q.indexOf(value)<initialQueue.indexOf(value)-2){
-      return 'Too Chaotic'
+    let newIndex=q.indexOf(value)
+    let originalIndex=initialQueue.indexOf(value)
+    if (newIndex<originalIndex-2){
+      return console.log('Too chaotic')
+    } 
+    
+    let delta=originalIndex-newIndex;
+    if (delta>0){
+      minBribes+=delta
     }
   }
-  return q
+  return console.log(minBribes)
 }
 
 // Max bribes =2
@@ -18,6 +26,14 @@ function minimum_bribes(q){
 let j= [1,5,2,3,4] // invalid
 // [1,2,3,4,5,6,7,8,9,10]
 // [1, 16, 32, 64, 128, 129, 256, 512, 513, 1024]
-let k= [16, 1, 32, 129, 128, 64, 512, 256, 1024, 513] // 5 bribes min
-
-console.log(minimum_bribes(k))
+// let k= [16, 1, 32, 129, 128, 64, 512, 256, 1024, 513] // 5 bribes min
+let m=[1, 2, 5, 3, 7, 8, 6, 4] // should be 7 min
+// [1,2,3,4,5,6,7,8]
+// [1,2,3,5,4,6,7,8]
+// [1,2,5,3,4,6,7,8]
+// [1,2,5,3,4,7,6,8]
+// [1,2,5,3,7,4,6,8]
+// [1,2,5,3,7,6,4,8]
+// [1,2,5,3,7,6,8,4]
+// [1,2,5,3,7,8,6,4] 7 bribes
+minimumBribes(m)
