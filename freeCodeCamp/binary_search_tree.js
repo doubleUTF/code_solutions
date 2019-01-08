1f757994e152cb5f5e22e1b5de40f9e045acccbd
BigD <dlau373@gmail.com>
2019-01-08 09:21:50 -0800

    WIP

--- freeCodeCamp/binary_search_tree.js
+++ freeCodeCamp/binary_search_tree.js
@@ -50,26 +50,214 @@ function BinarySearchTree() {
     }
 
     this.findMinHeight=()=>{
-      let cur=this.root;
-      if (!cur) return -1;
-      let temp;
-      while (cur){
-        
-      }
-
+      if (!this.root) return -1;
+      let height=0;
+      let nextNodes=[]
+      this.root.left? nextNodes.push(this.root.left) : ''
+      this.root.right? nextNodes.push(this.root.right) : ''
+      
+       while (nextNodes.length>0){
+         let tempNodes=[]
+         height++
+          for (let i=0; i<nextNodes.length; i++){
+            let nextNode= nextNodes[i];
+            if (!nextNode.left && !nextNode.right){
+              return height
+            }
+            nextNode.left? tempNodes.push(nextNode.left) : ''
+            nextNode.right? tempNodes.push(nextNode.right) : ''            
+          }
+          nextNodes=tempNodes;
+       }
+       return height
     }
     
     this.findMaxHeight=()=>{
-      
+      if (!this.root) return -1;
+      let height=0;
+      let nextNodes=[]
+      this.root.left? nextNodes.push(this.root.left) : '';
+      this.root.right? nextNodes.push(this.root.right) : '';
+
+      while (nextNodes.length>0){
+        height++;
+        let tempNodes=[];
+        for (let i=0; i<nextNodes.length;i++){
+          let curNode=nextNodes[i];
+          curNode.left ? tempNodes.push(curNode.left) : '';
+          curNode.right ? tempNodes.push(curNode.right) : '';
+        }
+        nextNodes=tempNodes;
+      }
+      return height;
     }
 
     this.isBalanced=()=>{
-      
+      let diff = this.findMaxHeight()-this.findMinHeight();
+      return  diff <=1 ? true : false;
     }
+
+    this.inorder=()=>{
+      // Left to right
+      let result=[]
+      if (!this.root) return null;
+      function recurInorder(node){
+        if (node.left) {
+          recurInorder(node.left)
+        }
+        result.push(node.value)
+        if (node.right){
+          recurInorder(node.right)
+        }
+      }
+      recurInorder(this.root);
+      return result;
+    }
+
+    this.preorder=()=>{
+      // Roots first
+      let result=[];
+      if (!this.root) return null;
+      function recurPreorder(node){
+        result.push(node.value);
+        if (node.left){
+          recurPreorder(node.left)
+        }
+        if(node.right){
+          recurPreorder(node.right)
+        }
+      }
+      recurPreorder(this.root)
+      return result;
+    }
+
+    this.postorder=()=>{
+      // Leaves first
+      let result=[];
+      if (!this.root) return null;
+      function recurPostorder(node){
+        if (node.left){
+          recurPostorder(node.left)
+        }
+        if (node.right){
+          recurPostorder(node.right)
+        }
+        result.push(node.value)
+      }
+      recurPostorder(this.root);
+      return result;
+    }
+
+    this.levelOrder=()=>{
+      if (!this.root) return null;
+      let result=[];
+      let nextNodes=[];
+      result.push(this.root.value);
+      this.root.left ? nextNodes.push(this.root.left) : '';
+      this.root.right ? nextNodes.push(this.root.right) : '';
+      while (nextNodes.length>0){
+        let tempNodes=[];
+        for (let i =0; i<nextNodes.length; i++){
+          let node=nextNodes[i];
+          result.push(node.value);
+          node.left ? tempNodes.push(node.left) : '';
+          node.right ? tempNodes.push(node.right) : ''; 
+        }
+        nextNodes=tempNodes;        
+      }
+      return result;
+    }
+
+    
+    this.reverseLevelOrder=()=>{
+      if (!this.root) return null;
+      let result=[];
+      let nextNodes=[];
+      result.push(this.root.value);
+      this.root.right ? nextNodes.push(this.root.right) : '';
+      this.root.left ? nextNodes.push(this.root.left) : '';
+      while (nextNodes.length>0){
+        let tempNodes=[];
+        for (let i =0; i<nextNodes.length; i++){
+          let node=nextNodes[i];
+          result.push(node.value);
+          node.right ? tempNodes.push(node.right) : ''; 
+          node.left ? tempNodes.push(node.left) : '';
+        }
+        nextNodes=tempNodes;        
+      }
+      return result;
+    }
+
+    this.remove=(num)=>{
+      let findResult=this.find(num);
+      if (!findResult || !findResult.node) return null;
+      if (!findResult.parent) {
+        findResult.node.left ? findResult.node.left : findResult.node.right;
+        return this.root=(findResult.node.left ? findResult.node.left : findResult.node.right);
+      }
+      let children=(findResult.node.left ? 1 : 0) + (findResult.node.right ? 1 : 0)
+      let direction;
+
+      if (findResult.parent.left){
+        if (findResult.parent.left.value==num){
+          direction='left';
+        } else {
+          direction='right';
+        }
+      } 
+      console.log(direction)
+        switch (children){
+          case 0:
+            findResult.parent[direction]=null;
+            break;
+          case 1:
+            findResult.parent.right= findResult.node.left ? findResult.node.left : findResult.node.right;
+            break;
+          case 2:
+        }
+    }
+
+    this.find=(int)=>{
+      // return object with parent and node itself
+      let cur=this.root;
+      let temp;
+      let previous;
+      if (!cur) return null;
+      let result=(node,parent)=>{
+        return {node,parent}
+      }
+      if (cur.value==int) return result(cur,null)
+      while (cur){
+        temp=cur;
+        if (cur.value==int) return result(temp,previous);
+        if (int>cur.value){
+          cur=cur.right
+        } else {
+          cur=cur.left;
+        }
+        previous=temp
+      }
+     return temp==int ? result(temp,previous) : null;
+    }
+
+    
 }
 
 let bs=new BinarySearchTree();
+
+
 bs.add(5)
-bs.add(7)
-bs.add(6)
-console.log(bs.findMinHeight())
+bs.add(4)
+bs.add(3)
+bs.remove(4)
+// bs.add(6)
+// bs.add(8)
+// bs.add(7)
+// bs.add(9)
+// bs.add(3)
+// bs.add(1)
+// bs.add(2)
+
+// bs.add(5); bs.add(3); bs.add(7); bs.add(6); bs.add(10); bs.add(12); bs.remove(3); bs.remove(12); bs.remove(10)
+console.log(bs.root)
