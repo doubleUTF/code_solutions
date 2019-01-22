2294a8a8b7ba7468dc91b9ca89ea8ed889871633
BigD <dlau373@gmail.com>
2019-01-21 23:40:26 -0800

    WIP

--- freeCodeCamp/max_heap.js
+++ freeCodeCamp/max_heap.js
@@ -4,16 +4,19 @@ var MaxHeap = function() {
     this.heap.push(value);
     let idx=this.heap.length-1;
     let newValue=this.heap[idx]
-    let parentIdx=idx/2;
+    let parentIdx=Math.floor(idx/2);
     let parentValue=this.heap[parentIdx];
-    console.log('parent value',parentValue)
     if (!parentValue) return;
-    console.log(newValue,parentValue)
+    // console.log(this.heap);
     while (newValue>parentValue){
-      [newValue,parentValue]=[parentValue,newValue];
+      [this.heap[idx],this.heap[parentIdx]]=[this.heap[parentIdx],this.heap[idx]];
       idx=parentIdx;
-      parentIdx=idx/2;
+      newValue=this.heap[parentIdx];
+      parentIdx=Math.floor(parentIdx/2)
+      parentValue=this.heap[parentIdx];
+      if (!parentValue) return
     }
+    // console.log(this.heap)
   }
   // change code below this line
   // change code above this line
@@ -23,6 +26,10 @@ let mh=new MaxHeap()
 mh.add(56)
 mh.add(78)
 mh.add(100)
-console.log(mh.heap)
+mh.add(120)
+mh.add(300)
+// console.log(mh.heap)
 
 let k=[1,2,3];
+let b=null;
+console.log(5>b)
